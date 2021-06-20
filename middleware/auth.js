ba1248dc3946bcf38b1dd05f4621e7565a31152b
jonathan_nsiah <jooankrah@gmail.com>
2021-06-20 22:31:16 +0000

    no message

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const auth = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    return jwt.verify(token, "GodistheGreatest", (err, decoded) => {
+    return jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
       if (err) {
         return res.status(500).send({ message: "Failed to Authenticate" });
       } else {
